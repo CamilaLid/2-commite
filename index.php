8e4be1ee456fa4e00570ca66e624b21108a7bb9e
ReginaldoJLima <reginaldojlima963@gmail.com>
2020-10-14 14:50:20 -0300

    modificação

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<p>commite 2</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hi</h1>
+</body>
+</html>
